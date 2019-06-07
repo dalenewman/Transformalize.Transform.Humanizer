7a05932b7bf50221719a216f45c28bb458b43054
Dale Newman <dalenewman@gmail.com>
2019-06-07 14:26:33 -0400

    update to latest transformalize

--- nuget/nuget.bat
+++ nuget/nuget.bat
@@ -1,5 +1,5 @@
 nuget pack Transformalize.Transform.Humanizer.Autofac.nuspec -OutputDirectory "c:\temp\modules"
 nuget pack Transformalize.Transform.Humanizer.nuspec -OutputDirectory "c:\temp\modules"
 REM 
-REM nuget push "c:\temp\modules\Transformalize.Transform.Humanizer.0.5.0-beta.nupkg" -source https://api.nuget.org/v3/index.json
-REM nuget push "c:\temp\modules\Transformalize.Transform.Humanizer.Autofac.0.5.0-beta.nupkg" -source https://api.nuget.org/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.Transform.Humanizer.0.6.6-beta.nupkg" -source https://api.nuget.org/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.Transform.Humanizer.Autofac.0.6.6-beta.nupkg" -source https://api.nuget.org/v3/index.json
--- src/Transformalize.Transform.Humanizer/Actions/HumanizeLabels.cs
+++ src/Transformalize.Transform.Humanizer/Actions/HumanizeLabels.cs
@@ -1,5 +1,4 @@
 ﻿using Humanizer;
-using Transformalize.Actions;
 using Transformalize.Contracts;
 using Action = Transformalize.Configuration.Action;
 
